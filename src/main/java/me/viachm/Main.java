9b73d53e6eb6c3164a3f9488d0f2708844f7cdc1
viachm <viacheslav.mukha@gmail.com>
2018-01-13 21:12:46 +0200

    Support parallel page crawling

--- src/main/java/me/viachm/Main.java
+++ src/main/java/me/viachm/Main.java
@@ -7,57 +7,48 @@ import org.jsoup.select.Elements;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class Main {
 
     private static final String BASE_URL = "http://loveread.ec/read_book.php";
     private static final String FILE_NAME = "book.txt";
-    private static Integer bookId = 70180;
+    private static Integer bookId = 1759;
 
     public static void main(String[] args) {
-        try (FileOutputStream out = new FileOutputStream(FILE_NAME)) {
-            System.out.println("Starting book download...");
-            int pagesNumber = getPagesNumber(getPageUrl(BASE_URL, bookId, 1));
-            for (int i = 1; i <= pagesNumber; i++) {
-                String url = getPageUrl(BASE_URL, bookId, i);
-                String text = parsePage(url);
-                out.write(text.getBytes());
-            }
-
-            System.out.println("Successfully downloaded a book.");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private static String parsePage(String url) {
-        StringBuilder text = new StringBuilder();
+        System.out.println("Starting book download...");
+        Integer numberOfPages = getNumberOfPages(getPageUrl(BASE_URL, bookId, 1));
+        ExecutorService executor = Executors.newFixedThreadPool(numberOfPages);
 
         try {
-            System.out.println("Processing url " + url);
-            Document document = Jsoup.connect(url).get();
-            Elements linksOnPage = document.getElementsByTag("div").not(".footer").select("p");
+            List<Callable<String>> crawlers = getPageCrawlers(BASE_URL, numberOfPages);
+            List<Future<String>> pages = executor.invokeAll(crawlers);
 
-            linksOnPage.forEach(element ->  {
-                text.append(element.text());
-                text.append("\n");
-            });
-        } catch (IOException e) {
-            System.err.println("Error while page parsing for url '" + url + "': " + e.getMessage());
+            writePagesToFile(FILE_NAME, pages);
+            System.out.println("Successfully downloaded a book.");
+        } catch (InterruptedException e) {
+            System.out.println("Error while book downloading: " + e);
+        } finally {
+            executor.shutdown();
         }
-
-        return text.toString();
     }
 
     private static String getPageUrl(String baseUrl, Integer bookId, Integer pageId) {
         return baseUrl + "?id=" + bookId + "&p=" + pageId;
     }
 
-    private static int getPagesNumber(String url) {
+    private static int getNumberOfPages(String url) {
         Set<Integer> pages = new HashSet<>();
+
         try {
             Document document = Jsoup.connect(url).get();
             Elements pageLinks = document.getElementsByClass("navigation").select("a");
@@ -76,4 +67,48 @@ public class Main {
 
         return Collections.max(pages);
     }
+
+    private static List<Callable<String>> getPageCrawlers(String baseUrl, Integer numberOfPages) {
+        List<Callable<String>> crawlers = new ArrayList<>();
+
+        for (int i = 1; i <= numberOfPages; i++) {
+            String url = getPageUrl(baseUrl, bookId, i);
+            crawlers.add(() -> {
+                StringBuilder text = new StringBuilder();
+
+                try {
+                    System.out.println("Processing url " + url);
+                    Document document = Jsoup.connect(url).get();
+                    Elements paragraphs = document.select("p").not("style");
+
+                    for (Element paragraph : paragraphs) {
+                        text.append(paragraph.text());
+                        text.append("\n");
+                    }
+                } catch (IOException e) {
+                    System.err.println("Error while page parsing for url '" + url + "': " + e.getMessage());
+                }
+
+                return text.toString();
+            });
+        }
+
+        return crawlers;
+    }
+
+    private static void writePagesToFile(String fileName, List<Future<String>> pages) {
+        try (FileOutputStream out = new FileOutputStream(fileName)) {
+            pages.forEach(page -> {
+                try {
+                    String text = page.get();
+                    out.write(text.getBytes());
+                } catch (InterruptedException | IOException | ExecutionException e) {
+                    e.printStackTrace();
+                }
+            });
+        } catch (IOException e) {
+            System.out.println("Error while writing pages to file: " + e);
+        }
+    }
+
 }
